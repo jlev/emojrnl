b5b69ef6414d37de5327522d11193df23b333a8e
Josh Levinger <josh@levinger.net>
2016-05-08 15:18:28 -0700

    🔥

--- journal/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class JournalConfig(AppConfig):
-    name = 'journal'
--- sms/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from sms.views import user_signup, sms_entry
-
-urlpatterns = [
-    url(r'^signup/(?P<phone_number>[\d]{10,14})/?$', user_signup),
-    url(r'^entry/$', sms_entry),
-]
