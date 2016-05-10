09a005db93db7b892f566990aaa0a889d62fb3aa
Josh Levinger <josh@levinger.net>
2016-05-10 11:51:15 -0700

    🔛

--- emojrnl/wsgi.py
+++ emojrnl/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.environ.get('DJANGO_SETTINGS_MODULE') is 'emojrnl.settings_production':
+if os.environ.get('DJANGO_SETTINGS_MODULE') == 'emojrnl.settings_production':
     os.environ['HTTPS'] = "on"
     from dj_static import Cling
     application = Cling(get_wsgi_application())
